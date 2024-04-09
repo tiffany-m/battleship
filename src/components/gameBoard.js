af77e2ab4e9d8e3348fae2922f9a9bd4eb90660f
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-04-09 16:02:19 -0400

    Update gameboard so each cell has an object full of data

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,25 +1,34 @@
+class Cell {
+  constructor() {
+  this.empty = true,
+  this.ship = null,
+  this.hit = false,
+  this.miss = false
+  }
+}
+
 class GameBoard {
   constructor(rows, cols) {
     (this.rows = rows), (this.cols = cols), (this.board = []);
   }
 
   buildBoard() {
+    const newCell = new Cell();
     for (let i = 0; i < this.rows; i++) {
       this.board.push([]);
       for (let j = 0; j < this.cols; j++) {
-        this.board[i].push(null);
+        this.board[i].push(newCell);
       }
     }
     return this.board;
   }
 
   receiveAttack(row, col) {
-    this.board[row][col] = "X"
-
+    this.board[row][col].hit = true;
   }
 }
 
-export { GameBoard }
+export { GameBoard };
 
 // let test = new GameBoard(10, 10)
 
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -8,9 +8,19 @@ describe("Game Board", () => {
     expect(gameBoard.rows).toBe(10);
     expect(gameBoard.cols).toBe(10);
     gameBoard.buildBoard();
-    expect(gameBoard.board[0][0]).toBe(null)
-    expect(gameBoard.board[9][9]).toBe(null);
+        expect(gameBoard.board[0][0]).toEqual({
+          empty: true,
+          ship: null,
+          hit: false,
+          miss: false,
+        });
+        expect(gameBoard.board[9][9]).toEqual({
+          empty: true,
+          ship: null,
+          hit: false,
+          miss: false,
+        });
     gameBoard.receiveAttack(0, 0);
-    expect(gameBoard.board[0][0]).toBe("X")
+    expect(gameBoard.board[0][0].hit).toBe(true)
   });
 });
