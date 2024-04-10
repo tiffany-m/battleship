f0256c4e2143a5ac6c0f686e0311cbcbff80c034
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-04-10 19:41:21 -0400

    Add placeShip() method to GameBoard and tests

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -1,9 +1,9 @@
 class Cell {
   constructor() {
-  this.empty = true,
-  this.ship = null,
-  this.hit = false,
-  this.miss = false
+    (this.empty = true),
+      (this.ship = null),
+      (this.hit = false),
+      (this.miss = false);
   }
 }
 
@@ -17,19 +17,61 @@ class GameBoard {
     for (let i = 0; i < this.rows; i++) {
       this.board.push([]);
       for (let j = 0; j < this.cols; j++) {
-        this.board[i].push(newCell);
+        this.board[i].push(new Cell());
       }
     }
     return this.board;
   }
 
-  receiveAttack(row, col) {
-    this.board[row][col].hit = true;
+  placeShip(ship, row, col, orientation) {
+    // Checks for placement beyound board edges.
+    if (orientation === "horizontal") {
+      if (col + ship.length > this.cols) {
+        return {
+          success: false,
+          message: `${ship.name} placement is out of bounds. Horizontal orientation would extend beyond game board's edge.`,
+        };
+      }
+    } else {
+      if (row + ship.length > this.rows) {
+        return {
+          success: false,
+          message: `${ship.name} placement is out of bounds. Vertical orientation would extend beyond game board's edge.`,
+        };
+      }
+    }
+    // Checks for exisiting ships.
+    if (orientation === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[row][col + i].empty === false) {
+          return {
+            success: false,
+            message: `Cannot place ${ship.name}: space already occupied by ${this.board[row][col + i].ship}.`,
+          };
+        }
+      }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[row + i][col].empty === false) {
+          return {
+            success: false,
+            message: `Cannot place ${ship.name}: space already occupied by ${this.board[row + i][col].ship}.`,
+          };
+        }
+      }
+    }
+    // Places ships.
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === "horizontal") {
+        this.board[row][col + i].empty = false;
+        this.board[row][col + i].ship = `${ship.name}`;
+      } else {
+        this.board[row + i][col].empty = false;
+        this.board[row + i][col].ship = `${ship.name}`;
+      }
+    }
+    return true;
   }
 }
 
 export { GameBoard };
-
-// let test = new GameBoard(10, 10)
-
-// console.log(test.buildBoard())
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -1,26 +1,132 @@
-import { GameBoard } from "../components/gameBoard.js";
+import { GameBoard } from "../components/gameboard.js";
 import { Ship } from "../components/ship.js";
 
-
-describe("Game Board", () => {
-  test("Game Board test", () => {
+describe("Creating Game Board", () => {
+  test("Game Board Creation Test", () => {
     let gameBoard = new GameBoard(10, 10);
     expect(gameBoard.rows).toBe(10);
     expect(gameBoard.cols).toBe(10);
     gameBoard.buildBoard();
-        expect(gameBoard.board[0][0]).toEqual({
-          empty: true,
-          ship: null,
-          hit: false,
-          miss: false,
-        });
-        expect(gameBoard.board[9][9]).toEqual({
-          empty: true,
-          ship: null,
-          hit: false,
-          miss: false,
-        });
-    gameBoard.receiveAttack(0, 0);
-    expect(gameBoard.board[0][0].hit).toBe(true)
+    expect(gameBoard.board[0][0]).toEqual({
+      empty: true,
+      ship: null,
+      hit: false,
+      miss: false,
+    });
+    expect(gameBoard.board[9][9]).toEqual({
+      empty: true,
+      ship: null,
+      hit: false,
+      miss: false,
+    });
+  });
+});
+
+describe("Ship Placement on Game Board", () => {
+  //  SETUP -------------------------------------------------------------------------------------------->
+  let gameBoard;
+  beforeEach(() => {
+    gameBoard = new GameBoard(10, 10);
+    gameBoard.buildBoard();
+  });
+  //  All 5 Ships -------------------------------------------------------------------------------------------->
+  describe("Place all 5 ships on board.", () => {
+    test("Test Carrier, Battleship, Cruiser, Submarine, Patrol Boat Placement", () => {
+      let carrier = new Ship("Carrier", 5);
+      let battleship = new Ship("Battleship", 4);
+      let cruiser = new Ship("Cruiser", 3);
+      let submarine = new Ship("Submarine", 3);
+      let patrolBoat = new Ship("Patrol Boat", 2);
+      gameBoard.placeShip(carrier, 1, 1, "horizontal");
+      expect(gameBoard.board[1][1]).toEqual(
+        expect.objectContaining({ ship: "Carrier" }),
+      );
+      expect(gameBoard.board[1][5]).toEqual(
+        expect.objectContaining({ ship: "Carrier" }),
+      );
+      gameBoard.placeShip(battleship, 3, 5, "vertical");
+      expect(gameBoard.board[3][5]).toEqual(
+        expect.objectContaining({ ship: "Battleship" }),
+      );
+      expect(gameBoard.board[6][5]).toEqual(
+        expect.objectContaining({ ship: "Battleship" }),
+      );
+      gameBoard.placeShip(cruiser, 8, 1, "horizontal");
+      expect(gameBoard.board[8][1]).toEqual(
+        expect.objectContaining({ ship: "Cruiser" }),
+      );
+      expect(gameBoard.board[8][3]).toEqual(
+        expect.objectContaining({ ship: "Cruiser" }),
+      );
+      gameBoard.placeShip(submarine, 2, 8, "vertical");
+      expect(gameBoard.board[2][8]).toEqual(
+        expect.objectContaining({ ship: "Submarine" }),
+      );
+      expect(gameBoard.board[4][8]).toEqual(
+        expect.objectContaining({ ship: "Submarine" }),
+      );
+      gameBoard.placeShip(patrolBoat, 9, 7, "horizontal");
+      expect(gameBoard.board[9][7]).toEqual(
+        expect.objectContaining({ ship: "Patrol Boat" }),
+      );
+      expect(gameBoard.board[9][8]).toEqual(
+        expect.objectContaining({ ship: "Patrol Boat" }),
+      );
+    });
+  });
+  // Horizontal Ship -------------------------------------------------------------------------------------------->
+  describe("Horizonal Ship Placement", () => {
+    test("Horizontal Test", () => {
+      let cruiser = new Ship("Cruiser", 3);
+      gameBoard.placeShip(cruiser, 8, 1, "horizontal");
+      expect(gameBoard.board[8][1]).toEqual(
+        expect.objectContaining({ ship: "Cruiser" }),
+      );
+      expect(gameBoard.board[8][2]).toEqual(
+        expect.objectContaining({ ship: "Cruiser" }),
+      );
+      expect(gameBoard.board[8][3]).toEqual(
+        expect.objectContaining({ ship: "Cruiser" }),
+      );
+    });
+  });
+  // Vertical Ship -------------------------------------------------------------------------------------------->
+  describe("Vertical Ship Placement", () => {
+    test("Vertical Test", () => {
+      let submarine = new Ship("Submarine", 3);
+      gameBoard.placeShip(submarine, 3, 5, "vertical");
+      expect(gameBoard.board[3][5]).toEqual(
+        expect.objectContaining({ ship: "Submarine" }),
+      );
+      expect(gameBoard.board[4][5]).toEqual(
+        expect.objectContaining({ ship: "Submarine" }),
+      );
+      expect(gameBoard.board[5][5]).toEqual(
+        expect.objectContaining({ ship: "Submarine" }),
+      );
+    });
+  });
+  // Out of Bounds or Overlapping -------------------------------------------------------------------------------------------->
+  describe("Out of Bounds or Overlapping Test Should Fail", () => {
+    let cruiser = new Ship("Cruiser", 3);
+    let submarine = new Ship("Submarine", 3);
+    test("Horizontal Out of Bounds Test", () => {
+      expect(gameBoard.placeShip(cruiser, 0, 9, "horizontal")).toEqual({
+        message:
+          "Cruiser placement is out of bounds. Horizontal orientation would extend beyond game board's edge.",
+      });
+    });
+    test("Vertical Out of Bounds Test", () => {
+      expect(gameBoard.placeShip(cruiser, 9, 0, "vertical")).toEqual({
+        message:
+          "Cruiser placement is out of bounds. Vertical orientation would extend beyond game board's edge.",
+      });
+    });
+    test("Overlapping Ships", () => {
+      gameBoard.placeShip(cruiser, 3, 3, "horizontal");
+      expect(gameBoard.placeShip(submarine, 2, 4, "vertical")).toEqual({
+        message: "Cannot place Submarine: space already occupied by Cruiser.",
+      });
+    });
   });
 });
