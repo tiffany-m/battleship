f62208a1dbb40cd8c540ad9bfc0f42aa60a49f17
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-05-20 16:33:06 -0400

    Put ships in arrays for cleaner code

--- src/components/gameLogic.js
+++ src/components/gameLogic.js
@@ -1,15 +1,31 @@
 import { Player } from "./player.js";
+import { Ship } from "./ship.js"
 import {
   humanGameBoard,
   computerGameBoard,
   chooseShipPlacementBtn,
 } from "./dom.js";
+import { pickNewShipPlacement } from "../index.js"
 let humanPlayer = new Player(false);
 let computerPlayer = new Player(true);
 let currentPlayer = humanPlayer;
 let computerPicks = [];
 let isGameOver = false;
-import { pickNewShipPlacement } from "../index.js"
+let humanShipsArr = []
+let computerShipsArr = []
+let carrierHuman = new Ship("Carrier", 5);
+let battleshipHuman = new Ship("Battleship", 4);
+let cruiserHuman = new Ship("Cruiser", 3);
+let submarineHuman = new Ship("Submarine", 3);
+let patrolBoatHuman = new Ship("Patrol Boat", 2);
+let carrierComp = new Ship("Carrier", 5);
+let battleshipComp = new Ship("Battleship", 4);
+let cruiserComp = new Ship("Cruiser", 3);
+let submarineComp = new Ship("Submarine", 3);
+let patrolBoatComp = new Ship("Patrol Boat", 2);
+
+humanShipsArr.push(carrierHuman, battleshipHuman, cruiserHuman, submarineHuman, patrolBoatHuman)
+computerShipsArr.push(carrierComp, battleshipComp, cruiserComp, submarineComp, patrolBoatComp)
 
 function gameReset() {
   modal.close();
@@ -157,4 +173,6 @@ export {
   humanPlayer,
   computerPlayer,
   currentPlayer,
+  humanShipsArr,
+  computerShipsArr
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { Ship } from "./components/ship.js";
 import {
   renderPlayerBoards,
   addListenersToCells,
   humanPlayer,
   computerPlayer,
   randomShipPlacement,
+  humanShipsArr,
+  computerShipsArr
 } from "./components/gameLogic.js";
 import {
   humanGameBoard,
@@ -14,27 +15,12 @@ import {
 } from "./components/dom.js";
 import "./styles.css";
 
-let carrierHuman = new Ship("Carrier", 5);
-let battleshipHuman = new Ship("Battleship", 4);
-let cruiserHuman = new Ship("Cruiser", 3);
-let submarineHuman = new Ship("Submarine", 3);
-let patrolBoatHuman = new Ship("Patrol Boat", 2);
-let carrierComp = new Ship("Carrier", 5);
-let battleshipComp = new Ship("Battleship", 4);
-let cruiserComp = new Ship("Cruiser", 3);
-let submarineComp = new Ship("Submarine", 3);
-let patrolBoatComp = new Ship("Patrol Boat", 2);
-
 function pickNewShipPlacement(player, gameBoard){
     player.gameBoardInstance.reset();
 
     randomShipPlacement(
       player,
-      carrierHuman,
-      battleshipHuman,
-      cruiserHuman,
-      submarineHuman,
-      patrolBoatHuman,
+      ...humanShipsArr
     );
 
     renderPlayerBoards(player, gameBoard);
@@ -42,20 +28,12 @@ function pickNewShipPlacement(player, gameBoard){
 
 randomShipPlacement(
   humanPlayer,
-  carrierHuman,
-  battleshipHuman,
-  cruiserHuman,
-  submarineHuman,
-  patrolBoatHuman,
+  ...humanShipsArr
 );
 
 randomShipPlacement(
   computerPlayer,
-  carrierComp,
-  battleshipComp,
-  cruiserComp,
-  submarineComp,
-  patrolBoatComp,
+  ...computerShipsArr
 );
 
 renderPlayerBoards(humanPlayer, humanGameBoard);
